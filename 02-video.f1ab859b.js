01c08e6d648ce1971d518cdccc66e0104686f139
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-03-08 12:40:25 +0000

    Deploying to gh-pages from @ IamVovaMoroz/parcel-project-HW8@5ebab3a0fd7e2df5b677f52790473e04800d3ecf 🚀

--- 02-video.f1ab859b.js
+++
@@ -1,2 +0,0 @@
-!function(){const n=document.querySelector("iframe"),e=new Vimeo.Player(n);e.on("play",(function(){console.log("played the video!")})),e.getVideoTitle().then((function(n){console.log("title:",n)}));e.on("play",(function(n){})),e.play().then((function(){})).catch((function(n){n.name}))}();
-//# sourceMappingURL=02-video.f1ab859b.js.map
