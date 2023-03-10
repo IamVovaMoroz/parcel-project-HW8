4920b2c75b3f137c7a82ba5b4cbf1139fa87d3ef
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-03-10 12:20:37 +0300

    3 task in process

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,11 +4,13 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form label');
+console.log(input);
 // const refs = {
 //   form: document.querySelector('.feedback-form'),
 //   textarea: document.querySelector('.feedback-form textarea'),
 // };
-
+populateForm();
 // Имя ключа в localStorage записываем в переменную
 const STORAGE_KEY = '"feedback-form-state';
 
@@ -28,7 +30,6 @@ form.addEventListener('input', event => {
   //   const formDataObject = localStorage.getItem((formData));
 });
 // refs.textarea.addEventListener('input', onTextareaInput);
-populateForm();
 
 function onFormSubmit (event) {
   event.preventDefault();
@@ -50,19 +51,24 @@ function onFormInput (event) {
   localStorage.setItem(STORAGE_KEY, inputValue);
 }
 
-// Получаем значение из LocalStorage и возвращаем назад его в
-// инпут(для не отправленных сообщений)
-
-function populateForm () {
+function populateForm (event) {
   const dataLocalStorage = localStorage.getItem(STORAGE_KEY);
   const savedMessage = JSON.parse(dataLocalStorage);
 
   //   чтобы небыло Null для пользователей кто не был ранее,
   //    делаем проверку на сохранённое сообщение.
   if (savedMessage) {
-    console.log(savedMessage);
+    // Значение   получает из LOcal сторедж,
+    //  если клиент не отправил значения
+    textarea.value = savedMessage.message;
 
-    // form.value = savedMessage;
+    // input.value = savedMessage.email;
+
+    console.log(savedMessage);
+    console.log();
+
+    // Получить доступ к инпуту и textarea и
+    // приравнять их к значениям из localStorage
 
     // Значение поля email из LocalStorage
     console.log(savedMessage.email);
@@ -72,9 +78,36 @@ function populateForm () {
   //   после перезагрузки страницы будет сохраняться текст в textarea
 }
 
+// Получаем значение из LocalStorage и возвращаем назад его в
+// инпут(для не отправленных сообщений)
+
+// function populateForm (event) {
+//   const dataLocalStorage = localStorage.getItem(STORAGE_KEY);
+//   const savedMessage = JSON.parse(dataLocalStorage);
+
+//   //   чтобы небыло Null для пользователей кто не был ранее,
+//   //    делаем проверку на сохранённое сообщение.
+//   if (savedMessage) {
+//     console.log(savedMessage);
+
+//     // Получить доступ к инпуту и textarea и
+//     // приравнять их к значениям из localStorage
+
+//     // form.email.value = savedMessage.email;
+//     // form.message.value = savedMessage.message;
+//     console.log(form.textarea);
+
+//     // Значение поля email из LocalStorage
+//     console.log(savedMessage.email);
+//     // Значение поля message из LocalStorage
+//     console.log(savedMessage.message);
+//   }
+//   //   после перезагрузки страницы будет сохраняться текст в textarea
+// }
+
 // Получить значение инпута email??
-console.log(form.email.name.value);
+
 // Получить значение form message
-console.log(form.textarea);
+// console.log(form.textarea);
 
 // Приравнять значение полей к значениям из LocalStorage в функции выше
